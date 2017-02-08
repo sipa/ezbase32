8e6fbaea2812037278af18c9870b4fead29599bb
Pieter Wuille <pieter.wuille@gmail.com>
2017-02-07 23:02:04 -0800

    fix

--- filter.sh
+++ filter.sh
@@ -3,8 +3,8 @@
 CONST="1-5,1-15,0 1-6,1-7,0 1-4,1-71,0 1-5,1-71,0.8912 1-5,1-59,0.8616 1-5,1-39,0.8118 1-8,1-71,1.073"
 
 ./crcmiddle $1 5 15 15 $CONST | tee phase_1.log |
-./crcmiddle $1 56 6  7  7 $CONST | tee phase_2.log |
-./crcmiddle $1 56 4 50 50 $CONST | tee phase_3.log |
+./crcmiddle $1 6  7  7 $CONST | tee phase_2.log |
+./crcmiddle $1 4 50 50 $CONST | tee phase_3.log |
 ./crcmiddle $1 6 15 15 $CONST | tee phase_4.log |
 ./crcmiddle $1 5 39 39 $CONST | tee phase_5.log |
 ./crcmiddle $1 4 71 71 $CONST | tee phase_6.log |
