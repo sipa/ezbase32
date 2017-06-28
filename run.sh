3bc3d998850f038a375c0cf9aabd2bdb31685ee5
Pieter Wuille <pieter.wuille@gmail.com>
2017-06-28 01:56:52 -0700

    New run script: let binary randomize

--- run.sh
+++ run.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-CHARSET="0123456789ABCDEFGHIJKLMNOPQRSTUV"
-
-function rnd() {
-  echo $(($(dd if=/dev/urandom bs=1 count=4 status=none | hexdump -e '"%u"') % $1))
-}
-
-function char() {
-  echo "${CHARSET:$(rnd 32):1}"
-}
-
-function rchar() {
-  echo "${CHARSET:$((1+$(rnd 31))):1}"
-}
-
 NUM=$(cat /proc/cpuinfo | fgrep processor | wc -l)
 
 for N in $(seq 1 $NUM); do
   while true; do
-    RND="$(char)$(char)$(char)$(char)$(char)$(char)$(char)$(char)$(char)$(char)$(char)$(rchar)"
-    echo "$RND: begin at @$(date "+%s")"
-    ./crccollide $RND 8 65
+    ./crccollide "" 8 64
   done &
 done
 
